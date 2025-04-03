9e3002cc958f8c36de2df0b9890faecc0556ae86
roodbarbosa <142645719+roodbarbosa@users.noreply.github.com>
2025-04-03 15:24:48 +0000

    Desafio aventureiro

---
+++ xadrezaventureiro.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    // Movimentando o cavalo
+   
+    for( int i = 1; i <= 1; i++){
+            for(int j = 1; j <= 2; j++ ){
+                printf("cima\n", i, j); // printa para "Cima"
+
+            }
+            printf("Direita\n"); // printa para "Direita"
+    }
+    
+
+ return 0;
+}
